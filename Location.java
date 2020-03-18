bd64867fe20ab34fe017d699e413f616d204255f
Samantha Swift <62332864+17swifts@users.noreply.github.com>
2020-03-18 16:38:52 +0100

    Update Location.java

--- Location.java
+++ Location.java
@@ -3,4 +3,24 @@ public class Location {
     private String name;
     private int ID;
     private int[] GPScoord;
+    
+    private static int count = 0;
+    
+    public Location(String name, int[] gps) {
+    	this.name = name;
+    	this.GPScoord = gps;
+    	this.ID = count++;
+    }
+    
+    public String getLocationName() {
+    	return name;
+    }
+    
+    public int getLocationID() {
+    	return ID;
+    }
+    
+    public int[] getGPScoord() {
+    	return GPScoord;
+    }
 }
