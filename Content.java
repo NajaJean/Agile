85ecf56bc008fca508e9dc5e77338bcc922967a5
s184376 <55983203+s184376@users.noreply.github.com>
2020-03-18 15:53:35 +0100

    Update Content.java

--- Content.java
+++ Content.java
@@ -1,5 +1,23 @@
 
-public class Content {
+public class Content extends Container {
 	private String name;
 	private Environment enviro;
+	private float threshold;
+	
+	public String getName() {
+    	return name;
+    }
+	public Environment getEnvironment() {
+    	return enviro;
+    }
+	public float getThreshold() {
+		return threshold;
+	}
+	
+	public Content(String name, Environment enviro, float threshold) {
+		this.name = name;
+		this.enviro = enviro;
+		this.threshold = threshold;
+	}
+	
 }
