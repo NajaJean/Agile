383a96dac0d9683274cd396a5fc01d66b6953c49
s184376 <55983203+s184376@users.noreply.github.com>
2020-03-18 16:33:12 +0100

    Update ContentTest.java

--- ContentTest.java
+++ ContentTest.java
@@ -11,13 +11,13 @@ public class ContentTest {
 
 	@Test
 	public void testContent() {
-		Content con1 = new Content("Banana",new Environment(5,1,85), 1.3f);
+		Content con1 = new Content("Banana",new Environment(5.0,1.0,0.85), 0.1);
 		
 		assertEquals(con1.getName(),"Banana");
-		assertEquals(con1.getEnvironment().getTemp(),5);
-		assertEquals(con1.getEnvironment().getPressure(),1);
-		assertEquals(con1.getEnvironment().getHumidity(),85);
-		assertEquals(con1.getThreshold(),1.3f, 0.0001);
+		assertEquals(con1.getEnvironment().getTemp(),5.0, 0.0001);
+		assertEquals(con1.getEnvironment().getPressure(),1.0, 0.0001);
+		assertEquals(con1.getEnvironment().getHumidity(),0.85, 0.0001);
+		assertEquals(con1.getThreshold(),0.1, 0.0001);
 		
 	}
 
