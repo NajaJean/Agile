1473d6b8656c45e1249246619a48acb60a610916
Samantha Swift <62332864+17swifts@users.noreply.github.com>
2020-03-18 14:31:12 +0100

    Update Container.java

--- Container.java
+++ Container.java
@@ -1,11 +1,45 @@
 
 public class Container {
-	private int length;
-	private int height;
-	private int width;
-	private int size = length*height*width;
 	
-	private String containerID; 
-
+	private int containerID; 
+	private Client client;
+	private Environment enviro;
+	private Content content;
 	
+	private static int count = 0;
+	
+	public Container(Client client,Environment enviro,Content content) {
+		this.containerID = count++;
+		this.client = client;
+		this.enviro = enviro;
+		this.content = content;
+	}
+	
+	public int getContainerID() {
+		return containerID;
+	}
+	
+	public Client getClientofContainer() {
+		return client;
+	}
+	
+	public void setClientofContainer(Client client) {
+		this.client = client;
+	}
+	
+	public Environment getContainerEnvironment() {
+		return enviro;
+	}
+	
+	public void setContainerEnvironment(Environment enviro) {
+		this.enviro = enviro;
+	}
+	
+	public Content getContainerContent() {
+		return content;
+	}
+	
+	public void setContainerContent(Content content) {
+		this.content = content;
+	}
 }
