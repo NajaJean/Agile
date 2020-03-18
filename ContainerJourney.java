f77be46b256003302c198ca4fed310fe6d32c772
Samantha Swift <62332864+17swifts@users.noreply.github.com>
2020-03-18 14:41:21 +0100

    Update ContainerJourney.java

--- ContainerJourney.java
+++ ContainerJourney.java
@@ -1,4 +1,20 @@
 
 public class ContainerJourney {
+	private int journeyID;
+	private Location start;
+	private Location end;
+	private Container container;
 	
+	private static int count = 0;
+	
+	public ContainerJourney(Location start, Location end, Container container) {
+		this.journeyID = count++;
+		this.start = start;
+		this.end = end;
+		this.container = container;
+	}
+	
+	public int getJourneyID() {
+		return journeyID;
+	}
 }
