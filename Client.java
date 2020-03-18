c6e5ad7e212791833dce4f1eced0fa5e3ad59606
s184376 <55983203+s184376@users.noreply.github.com>
2020-03-18 13:38:49 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -4,5 +4,35 @@ public class Client {
     private String email;
     private String address;
     private int ID;
-
+    private static int count = 0;
+    
+    public String getName() {
+    	return name;
+    }
+    public void setName(String name) {
+    	this.name = name;
+    }
+    public String getEmail() {
+    	return email;
+    }
+    public void setEmail(String email) {
+    	this.email = email;
+    }
+    public String getAddress() {
+    	return address;
+    }
+    public void setAddress() {
+    	this.address = address;
+    }
+    public String getID() {
+    	return ID;
+    }
+    
+    public Client(String name, String email, String address, int ID) {
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.ID = count++;
+	}
+    
 }
