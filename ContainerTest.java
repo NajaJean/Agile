02d1aff3aca013cfa0d607b4310225eeb835c739
s184376 <55983203+s184376@users.noreply.github.com>
2020-03-20 13:42:04 +0100

    Update ContainerTest.java

--- ContainerTest.java
+++ ContainerTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import core.Client;
@@ -10,17 +11,27 @@ import core.Content;
 import core.Environment;
 
 public class ContainerTest {
-
+	
+	private Client cl1;
+	private Client cl2;
+	private Environment e1;
+	private Environment e2;
+	private Content con1;
+	private Content con2;
+	
+	@Before 
+	public void createContainers() {
+		cl1 = new Client("Mathilde","mathildesemail@gmail.com","Anker Egelundsvej 1");
+		e1 = new Environment(5.3,1.1,0.85);
+		con1 = new Content("Banana",new Environment(5.0,1.0,0.85), 0.1);
+		
+		cl2 = new Client("Naja","najasemail@gmail.com","Rådhuspladsen 100");
+		e2 = new Environment(2.1,0.9,0.55);
+		con2 = new Content("Apple",new Environment(2.0,0.8,0.5), 0.1);
+	}
+	
 	@Test
-	public void testContainer() {
-		Client cl1 = new Client("Mathilde","mathildesemail@gmail.com","Anker Egelundsvej 1");
-		Environment e1 = new Environment(5.3,1.1,0.85);
-		Content con1 = new Content("Banana",new Environment(5.0,1.0,0.85), 0.1);
-		
-		Client cl2 = new Client("Naja","najasemail@gmail.com","Rådhuspladsen 100");
-		Environment e2 = new Environment(2.1,0.9,0.55);
-		Content con2 = new Content("Apple",new Environment(2.0,0.8,0.5), 0.1);
-		
+	public void testContainer() {	
 		Container c1 = new Container(cl1, e1, con1);
 		Container c2 = new Container(cl2, e2, con2);
 		
@@ -30,6 +41,11 @@ public class ContainerTest {
 		assertEquals(cl1,c1.getClientofContainer());
 		assertEquals(e1,c1.getContainerEnvironment());
 		assertEquals(con1,c1.getContainerContent());
+	}
+	
+	@Test
+	public void testConfigureContainer() {
+		Container c1 = new Container(cl1, e1, con1);
 		
 		c1.setClientofContainer(cl2);
 		assertEquals(cl2,c1.getClientofContainer());
