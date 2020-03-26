ba85b73e2f1154637c4d4523071d0907783171a8
Samantha Swift <62332864+17swifts@users.noreply.github.com>
2020-03-26 14:21:53 +1000

    Update Login.java

--- src/core/Login.java
+++ src/core/Login.java
@@ -6,17 +6,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
  
 class LoginFrame extends JFrame implements ActionListener {
-	String CorL;
+ 
     Container container = getContentPane();
     JLabel userNameLabel = new JLabel ("User Name:");
     JLabel passwordLabel = new JLabel ("Password:");
     JButton loginButton = new JButton ("Login");
     JButton cancelButton = new JButton ("Cancel");
+    JButton newClientButton = new JButton ("Create new Client");
+    JButton newManagerButton = new JButton ("Create new Manager");;
     JTextField userTextField = new JTextField (5);
     JPasswordField passwordTextField = new JPasswordField (5);
  
-    LoginFrame(String CorL) {
-    	this.CorL = CorL;
+    LoginFrame() {
         setLayoutManager();
         setLocationAndSize();
         addComponentsToContainer();
@@ -33,6 +34,8 @@ class LoginFrame extends JFrame implements ActionListener {
         passwordLabel.setBounds (15, 35, 100, 25);
         loginButton.setBounds (10, 70, 100, 25);
         cancelButton.setBounds (105, 70, 100, 25);
+        newClientButton.setBounds (30, 105, 150, 25);
+        newManagerButton.setBounds (30, 135, 150, 25);
         userTextField.setBounds (100, 10, 100, 25);
         passwordTextField.setBounds (100, 35, 100, 25);
     }
@@ -42,6 +45,8 @@ class LoginFrame extends JFrame implements ActionListener {
     	container.add(passwordLabel);
     	container.add(loginButton);
     	container.add(cancelButton);
+    	container.add(newClientButton);
+        container.add(newManagerButton);
     	container.add(userTextField);
         container.add(passwordTextField);
     }
@@ -49,62 +54,61 @@ class LoginFrame extends JFrame implements ActionListener {
     public void addActionEvent() {
         loginButton.addActionListener(this);
         cancelButton.addActionListener(this);
+        newClientButton.addActionListener(this);
+        newManagerButton.addActionListener(this);
     }
  
  
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        //Coding Part of LOGIN button
         if (e.getSource() == loginButton) {
             String userText;
             String pwdText;
             userText = userTextField.getText();
             pwdText = passwordTextField.getText();
-            
-            if (CorL=="Client") {
-	            if (userText.equalsIgnoreCase("corona") && pwdText.equalsIgnoreCase("1234")) {
-	                JOptionPane.showMessageDialog(this, "Login Successful");
-	                openClientMenu();
-	            } else {
-	                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
-	            }
+            if (userText.equalsIgnoreCase("corona") && pwdText.equalsIgnoreCase("1234")) {
+                JOptionPane.showMessageDialog(this, "Login Successful");
+            } 
+            else {
+                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
             }
-            if (CorL=="Logistic Company") {
-	            if (userText.equalsIgnoreCase("corona") && pwdText.equalsIgnoreCase("1234")) {
-	                JOptionPane.showMessageDialog(this, "Login Successful");
-	                openLogisticCompanyMenu();
-	            } else {
-	                JOptionPane.showMessageDialog(this, "Invalid Username or Password");
-	            }
-            }
+ 
         }
+        //Coding Part of RESET button
         if (e.getSource() == cancelButton) {
             userTextField.setText("");
             passwordTextField.setText("");
         }
+        if (e.getSource() == newClientButton) {
+        	CreateUserFrame c = new CreateUserFrame();
+        	c.setTitle("Create Client");
+            c.setVisible(true);
+            c.setBounds(10, 10, 370, 600);
+            c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            c.setResizable(false);
+        }
+        if (e.getSource() == newManagerButton) {
+        	CreateUserFrame c = new CreateUserFrame();
+        	c.setTitle("Create Manager");
+            c.setVisible(true);
+            c.setBounds(10, 10, 370, 600);
+            c.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            c.setResizable(false);
+        }
     }
-    public void openClientMenu() {
-    	JFrame frame = new JFrame("Client Menu");
-        frame.setTitle("Client Menu");
-        frame.getContentPane().add (new ClientMenuFrame());
-        frame.setVisible(true);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-    }
-    
-    public void openLogisticCompanyMenu() {
-    	
-    }
+ 
 }
  
 public class Login {
     public static void main(String[] a) {
-        LoginFrame frame = new LoginFrame("");
+        LoginFrame frame = new LoginFrame();
         frame.setTitle("Login Form");
         frame.setVisible(true);
         frame.setBounds(10, 10, 370, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
+ 
     }
+ 
 }
