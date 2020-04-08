cc00d055e788107dc1cfa673c1f44d3629d0e339
NajaJean <naja-jean@live.dk>
2020-04-08 11:07:00 +0200

    Update StepDefs_ClientBookContainer.java

--- src/stepDefinitions/StepDefs_ClientBookContainer.java
+++ src/stepDefinitions/StepDefs_ClientBookContainer.java
@@ -1,5 +1,48 @@
 package stepDefinitions;
 
+import core.Database;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class StepDefs_ClientBookContainer {
+	
+	Database b = new Database("C:\\Users\\-\\eclipse-workspace\\Agile\\target\\agileProject.accdb");
+	
+	@Given("that there exists an empty container in the database")
+	public void that_there_exists_an_empty_container_in_the_database() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the Client books a container")
+	public void the_Client_books_a_container() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the first empty container existing in the database should be assigned to the Client")
+	public void the_first_empty_container_existing_in_the_database_should_be_assigned_to_the_Client() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("that there does not exist an empty container in the database")
+	public void that_there_does_not_exist_an_empty_container_in_the_database() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the Client tries to book a container")
+	public void the_Client_tries_to_book_a_container() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("an error-message is displayed")
+	public void an_error_message_is_displayed() {
+	    System.out.println("We havn't got any containers available at the moment");
+	}
+
 
 }
