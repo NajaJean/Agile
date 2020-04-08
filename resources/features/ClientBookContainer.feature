fb6805eca6c8727d5d480145af33fa6d35f6ecd2
Mathilde Tannebæk <s184376@student.dtu.dk>
2020-04-08 15:46:33 +0200

    step definition client book

--- resources/features/ClientBookContainer.feature
+++ resources/features/ClientBookContainer.feature
@@ -7,8 +7,9 @@ Feature: Client, Book Container
 		Given that there exists an empty container in the database 
 		When the Client books a container
 		Then the first empty container existing in the database should be assigned to the Client
+		Then message is displayed saying "Container is succesfully booked"
 
 	Scenario: Client is not able to book container 
 		Given that there does not exist an empty container in the database 
 		When the Client tries to book a container
-		Then an error-message is displayed
+		Then an error-message is displayed saying "No empty containers available"
--- src/stepDefinitions/StepDefs_ClientBookContainer.java
+++ src/stepDefinitions/StepDefs_ClientBookContainer.java
@@ -16,39 +16,68 @@ import io.cucumber.java.en.When;
 public class StepDefs_ClientBookContainer {
 	
 	Database dC = new Database("C:\\Users\\-\\eclipse-workspace\\Agile\\target\\agileProject.accdb");
-	Database dI = new Database();
+	Database dI = new Database("C:\\Users\\-\\eclipse-workspace\\Agile\\target\\agileProject.accdb");
 	int idC;
 	int idI;
 	Container con;
 	
-	Client[] Clients;
-	Environment[] Enviros;
-	Content[] Contents;
-	Container[] Containers; 
+	Client[] ClientsC;
+	Environment[] EnvirosC;
+	Content[] ContentsC;
+	Container[] ContainersC; 
+	
+	Client[] ClientsI;
+	Environment[] EnvirosI;
+	Content[] ContentsI;
+	Container[] ContainersI; 
 	
 	NotifyObject response;
 	
-	public void import_database() {
-		String[][] clients = dC.getTable("Clients");
-		Clients = new Client[clients.length];
-		for(int i = 0; i < clients.length; i++) {
-			Clients[i] = new Client(clients[i][4],clients[i][5],clients[i][1],clients[i][2],clients[i][3]);
+	public void import_databaseC() {
+		String[][] clientsC = dC.getTable("Clients");
+		ClientsC = new Client[clientsC.length];
+		for(int i = 0; i < clientsC.length; i++) {
+			ClientsC[i] = new Client(clientsC[i][4],clientsC[i][5],clientsC[i][1],clientsC[i][2],clientsC[i][3]);
 		}
-		String[][] environments = dC.getTable("Environments");
-		Enviros = new Environment[environments.length];
-		for(int i = 0; i < environments.length; i++) {
-			Enviros[i] = new Environment(Double.parseDouble(environments[i][1]),Double.parseDouble(environments[i][2]),Double.parseDouble(environments[i][3]));
+		String[][] environmentsC = dC.getTable("Environments");
+		EnvirosC = new Environment[environmentsC.length];
+		for(int i = 0; i < environmentsC.length; i++) {
+			EnvirosC[i] = new Environment(Double.parseDouble(environmentsC[i][1]),Double.parseDouble(environmentsC[i][2]),Double.parseDouble(environmentsC[i][3]));
 		}
-		String[][] contents = dC.getTable("Contents");
-		Contents = new Content[contents.length];
-		for(int i = 0; i < contents.length; i++) {
-			Contents[i] = new Content(contents[i][1],Environment.findEnviro(contents[i][2],Enviros),Double.parseDouble(contents[i][3]));
+		String[][] contentsC = dC.getTable("Contents");
+		ContentsC = new Content[contentsC.length];
+		for(int i = 0; i < contentsC.length; i++) {
+			ContentsC[i] = new Content(contentsC[i][1],Environment.findEnviro(contentsC[i][2],EnvirosC),Double.parseDouble(contentsC[i][3]));
 		}
 		
-		String[][] containers = dC.getTable("Contatiners");
-		Containers = new Container[containers.length];
-		for(int i = 0; i < containers.length; i++) {
-			Containers[i] = new Container(Client.findClient(containers[i][1],Clients),Environment.findEnviro(contents[i][2],Enviros),Content.findContent(containers[i][3],Contents));
+		String[][] containersC = dC.getTable("Contatiners");
+		ContainersC = new Container[containersC.length];
+		for(int i = 0; i < containersC.length; i++) {
+			ContainersC[i] = new Container(Client.findClient(containersC[i][1],ClientsC),Environment.findEnviro(contentsC[i][2],EnvirosC),Content.findContent(containersC[i][3],ContentsC));
+		}
+	}
+	
+	public void import_databaseI() {
+		String[][] clientsI = dI.getTable("Clients");
+		ClientsI = new Client[clientsI.length];
+		for(int i = 0; i < clientsI.length; i++) {
+			ClientsI[i] = new Client(clientsI[i][4],clientsI[i][5],clientsI[i][1],clientsI[i][2],clientsI[i][3]);
+		}
+		String[][] environmentsI = dI.getTable("Environments");
+		EnvirosI = new Environment[environmentsI.length];
+		for(int i = 0; i < environmentsI.length; i++) {
+			EnvirosI[i] = new Environment(Double.parseDouble(environmentsI[i][1]),Double.parseDouble(environmentsI[i][2]),Double.parseDouble(environmentsI[i][3]));
+		}
+		String[][] contentsI = dI.getTable("Contents");
+		ContentsI = new Content[contentsI.length];
+		for(int i = 0; i < contentsI.length; i++) {
+			ContentsI[i] = new Content(contentsI[i][1],Environment.findEnviro(contentsI[i][2],EnvirosI),Double.parseDouble(contentsI[i][3]));
+		}
+		
+		String[][] containersI = dI.getTable("Contatiners");
+		ContainersI = new Container[containersI.length];
+		for(int i = 0; i < containersI.length; i++) {
+			ContainersI[i] = new Container(Client.findClient(containersI[i][1],ClientsI),Environment.findEnviro(contentsI[i][2],EnvirosI),Content.findContent(containersI[i][3],ContentsI));
 		}
 	}
 	
@@ -60,14 +89,19 @@ public class StepDefs_ClientBookContainer {
 
 	@When("the Client books a container")
 	public void the_Client_books_a_container() {
-		con = Container.findContainer(Integer.toString(idC), Containers);
-		con.setClientofContainer(Clients[1]);
+		con = Container.findContainer(Integer.toString(idC), ContainersC);
+		con.setClientofContainer(ClientsC[1]);
 		response = con.checkBookingOfContainer(idC);
 	}
 
 	@Then("the first empty container existing in the database should be assigned to the Client")
 	public void the_first_empty_container_existing_in_the_database_should_be_assigned_to_the_Client() {
-	    assertEquals(con.getClientofContainer(),Clients[1].getID());
+	    assertEquals(con.getClientofContainer(),ClientsC[1].getID());
+	}
+	
+	@Then("message is displayed saying {string}")
+	public void message_is_displayed_saying(String s) {
+		assertEquals(s, response.getNotifyMessage());
 	}
 
 	@Given("that there does not exist an empty container in the database")
@@ -81,11 +115,9 @@ public class StepDefs_ClientBookContainer {
 		response = con.checkBookingOfContainer(idI);
 	}
 
-	@Then("an error-message is displayed")
-	public void an_error_message_is_displayed() {
-		// not done yet
-		assertEquals("Client is notified that no container is available", response.getNotifyMessage());
-	    //System.out.println("We haven't got any containers available at the moment");
+	@Then("an error-message is displayed saying {string}")
+	public void an_error_message_is_displayed(String s) {
+		assertEquals(s, response.getNotifyMessage());
 	}
 
 
