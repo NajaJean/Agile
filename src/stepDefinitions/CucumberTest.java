a7b0076c1fea3d7d5374e9fc501211d37999b839
NajaJean <naja-jean@live.dk>
2020-04-13 19:01:51 +0200

    AlmostFixedCucumber

--- src/stepDefinitions/CucumberTest.java
+++ src/stepDefinitions/CucumberTest.java
@@ -1,10 +1,11 @@
+package stepDefinitions;
 
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/main/resources/")
+@CucumberOptions(features = "resources/")
 public class CucumberTest {
 
 }
--- src/stepDefinitions/StepDefs_ClientBookContainer2.java
+++ src/stepDefinitions/StepDefs_ClientBookContainer2.java
@@ -1,12 +1,10 @@
+package stepDefinitions;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import core.Client;
-import core.Container;
-import core.Content;
 import core.Database;
-import core.Environment;
-import core.NotifyObject;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
