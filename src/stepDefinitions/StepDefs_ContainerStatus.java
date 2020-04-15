b521169af8fe610872bed0d36addebb87f14ca06
NajaJean <naja-jean@live.dk>
2020-04-15 14:24:47 +0200

    Update StepDefs_ContainerStatus.java

--- src/stepDefinitions/StepDefs_ContainerStatus.java
+++ src/stepDefinitions/StepDefs_ContainerStatus.java
@@ -1,5 +1,32 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class StepDefs_ContainerStatus {
 
+	@Given("a client with a container")
+	public void a_client_with_a_container() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("a logistic company")
+	public void a_logistic_company() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("a logistic company updates the current status of the container")
+	public void a_logistic_company_updates_the_current_status_of_the_container() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the updates should be visible for the client")
+	public void the_updates_should_be_visible_for_the_client() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }
