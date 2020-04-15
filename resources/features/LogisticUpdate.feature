260682db7ab28bd1f7c5163e2385fbb6b3c7239e
Andrew <62332857+Andrew00000@users.noreply.github.com>
2020-04-15 13:07:47 +0200

    company update

--- resources/features/LogisticUpdate.feature
+++ resources/features/LogisticUpdate.feature
@@ -3,8 +3,15 @@ Feature: Logistic Company, Update container status
 	Actor: Logistic Company
 
 	Scenario: Logistic Company is able to update the status of container
-		Given the company selected a container
+		Given company selected a container
 		When the company updates the content
 		Then the content is updated
 		And message is displayed saying "Content update successful"
+		
+	Scenario: Logistic Company is able to update the status of container
+		Given the company selected a container journey
+		When the company updates the current location
+		Then the current location is updated
+		And than a message is displayed saying "The current location update was successful"
 
+	
--- src/stepDefinitions/StepDefs_LogisticUpdate.java
+++ src/stepDefinitions/StepDefs_LogisticUpdate.java
@@ -6,8 +6,10 @@ import static org.junit.Assert.assertTrue;
 
 import core.Client;
 import core.Container;
+import core.ContainerJourney;
 import core.Content;
 import core.Environment;
+import core.Location;
 import core.NotifyObject;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -23,6 +25,21 @@ public class StepDefs_LogisticUpdate {
 	
 	NotifyObject response;
 	
+	double[] cphgpscoords = {730.0, 128.0};
+	double[] tokyogpscoords = {1423.0, 245.0};
+	double[] sydneygpscoords = {1495.0, 750.0};
+	double[] nygpscoords = {290.0, 225.0};
+	double[] hawaiigpscoords = {1735.0, 265.0};
+	double[] riogpscoords = {420.0, 670.0};
+	
+
+	Location CPH = new Location("Copenhagen", cphgpscoords);
+	Location Tokyo = new Location("Tokyo", tokyogpscoords);
+	Location Sydney = new Location("Sydney", sydneygpscoords);
+	Location NY = new Location("New York", nygpscoords);
+	Location Hawaii = new Location("Hawaii", hawaiigpscoords);
+	Location Rio = new Location("Rio", riogpscoords);
+
 	Environment enviro = new Environment(5.0, 5.0, 5.0);
 	Environment newEnviro = new Environment(15.0, 15.0, 15.0);
 	
@@ -32,16 +49,21 @@ public class StepDefs_LogisticUpdate {
 	Content newStuff = new Content("NewStuff", newEnviro, 2.0);
 	
 	Container selectedC = new Container(client, enviro, stuff);
+
+	ContainerJourney containerJ = new ContainerJourney(CPH, NY, selectedC);
+	ContainerJourney selectedJ = containerJ;
 	
-	@Given("the company selected a container")
-	public void the_company_selected_a_container() {
+	
+	@Given("company selected a container")
+	public void company_selected_a_container() {
 		assertEquals(client.getID(), selectedC.getClientofContainer().getID());
 	}
 
 	@When("the company updates the content")
 	public void the_company_updates_the_content() {
-		response = selectedC.setContainerContent(newStuff);
-		context.setResponse(response);
+		selectedC.setContainerContent(newStuff);
+		//response = selectedC.setContainerContent(newStuff); //I dont know what this is :\
+		//context.setResponse(response);
 	}
 	
 
@@ -51,14 +73,41 @@ public class StepDefs_LogisticUpdate {
 		assertEquals(newStuff.getContentID(), selectedC.getContainerContent().getContentID());
 		assertEquals(newStuff.getName(), selectedC.getContainerContent().getName());
 		assertTrue(newStuff.getThreshold() == selectedC.getContainerContent().getThreshold());
+		assertEquals(newStuff.getEnvironment().getEnviro_ID(), selectedC.getContainerContent().getEnvironment().getEnviro_ID());
+		assertTrue(newStuff.getEnvironment().getHumidity() == selectedC.getContainerContent().getEnvironment().getHumidity());//assertEquals double double was outdated
+		assertTrue(newStuff.getEnvironment().getPressure() == selectedC.getContainerContent().getEnvironment().getPressure());//assertEquals double double was outdated
+		assertTrue(newStuff.getEnvironment().getTemp() == selectedC.getContainerContent().getEnvironment().getTemp());//assertEquals double double was outdated
+	}
+	
+	@Then("a message is displayed saying {string}") 
+	public void a_message_is_displayed_saying(String s){
+		System.out.println(response.getNotifyMessage());
+		assertEquals(s, response.getNotifyMessage());
+	}
+	
+	@Given("that the company selected a container journey")
+	public void that_the_company_selected_a_container_journey() {
+		assertEquals(containerJ.getJourneyID(), selectedJ.getJourneyID());
+	}
+
+	@When("the company updates the current location")
+	public void the_company_updates_the_current_location() {
+		selectedJ.setCurrentLocation(Hawaii);
+	}
+	
+
+	@Then("the current location is updated")
+	public void the_current_location_is_updated() {
 		
-		// The contents environment requirement do not need to be equal to the environment of the container. 
-		// The container could be outside of the environment threshold of the contents for instance
-		// Should delete 
-		
-		//assertEquals(newStuff.getEnvironment().getEnviro_ID(), selectedC.getContainerEnvironment().getEnviro_ID());
-		//assertTrue(newStuff.getEnvironment().getHumidity() == selectedC.getContainerEnvironment().getHumidity());
-		//assertTrue(newStuff.getEnvironment().getPressure() == selectedC.getContainerEnvironment().getPressure());
-		//assertTrue(newStuff.getEnvironment().getTemp() == selectedC.getContainerEnvironment().getTemp());
+		assertEquals(Hawaii.getLocationName(), selectedJ.getCurrentLocation().getLocationName());
+		assertEquals(Hawaii.getGPScoord(), selectedJ.getCurrentLocation().getGPScoord());
+		assertTrue(Hawaii.getGPScoord()[0] == selectedJ.getCurrentLocX());
+		assertTrue(Hawaii.getGPScoord()[1] == selectedJ.getCurrentLocY());
+	}
+	
+	@Then("than a message is displayed saying {string}") 
+	public void than_a_message_is_displayed_saying(String s){
+		System.out.println(response.getNotifyMessage());
+		assertEquals(s, response.getNotifyMessage());
 	}
 }
