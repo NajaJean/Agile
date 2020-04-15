085ec756339b58d66e6477ae43986c64c998eff9
Ellen Luke <ellencluke@gmail.com>
2020-04-15 16:04:42 +0200

    cucumber

--- src/stepDefinitions/StepDefs_LogisticCompCreateClient.java
+++ src/stepDefinitions/StepDefs_LogisticCompCreateClient.java
@@ -1,37 +1,50 @@
 package stepDefinitions;
 
+import static org.junit.Assert.assertTrue;
+
+import core.Client;
+import core.Database;
+import core.LogisticCompany;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+
 public class StepDefs_LogisticCompCreateClient {
+	
+	Database d = new Database("agileProject.accdb");
+	Client C;
+	LogisticCompany l; 
+	
+	
 	@Given("a logistic company")
 	public void a_logistic_company() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	   l = new LogisticCompany();
+	    
 	}
 
 	@Given("given a new client")
 	public void given_a_new_client() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		C = new Client("fred", "1234", "Fred Wesley", "freddie45@gmail.com","45 Harrison ct");
+	    
 	}
 
 	@When("a logistic company wants to create a new client")
 	public void a_logistic_company_wants_to_create_a_new_client() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		d.addToDatabase("Clients", C.toString());
+	    
 	}
 
 	@Then("the client is created and message displayed says {string}")
 	public void the_client_is_created_and_message_displayed_says(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		// need notify
+	    System.out.print("Client successfully created!");
+	    
 	}
 	
 	@Then("is found in the database")
 	public void is_found_in_the_database() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		assertTrue(d.checkUser(C.getUserName(), C.getPassword()));
+	    
 	}
 }
