22d9642d868785b9acf2b663ce2530e14dbc82b1
Samantha Swift <62332864+17swifts@users.noreply.github.com>
2020-04-19 09:48:55 +1000

    New step def

--- resources/features/LogisticRegisterContainer.feature
+++ resources/features/LogisticRegisterContainer.feature
@@ -4,11 +4,6 @@ Feature: Logistic Company, Register Container
 
 Scenario: Logistic Company is able to register container
 	Given that Logistic Company wants to register a container
-	When the Logistic Company insert all information (Current environment and current location)
-	Then the container is created with no content in the database
-	And a message “Container successfully registered” is displayed.
-
-Scenario: Logistic Company is not able to register container 
-	Given that Logistic Company wants to register a container
-	When the Logistic Company does not insert all information (Current environment and current location)
-	Then the container registering fails with a message “All fields has to be filled”
+	When the Logistic Company inserts all information
+	Then the container is created with no content in the database and message displayed saying “Container successfully registered”
+	And container is found in the database
---
+++ src/stepDefinitions/StepDefs_LogisticRegisterContainer.java
@@ -0,0 +1,41 @@
+package stepDefinitions;
+
+import core.Container;
+import core.Database;
+import core.Location;
+import core.LogisticCompany;
+import core.Environment;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefs_LogisticRegisterContainer {
+	Database d = new Database("agileProject.accdb");
+	Container C;
+	LogisticCompany l; 
+	
+	Location loc;
+	Environment enviro;
+	
+	@Given("that Logistic Company wants to register a container")
+	public void that_Logistic_Company_wants_to_register_a_container() {
+		l = new LogisticCompany();
+	}
+	@When("the Logistic Company inserts all information")
+	public void the_Logistic_Company_inserts_all_information() {
+		enviro = new Environment(5.3,1.1,0.85);
+		double[] cphgps = {55.1, 12.6};
+		loc = new Location("Copenhagen",cphgps);
+	}
+	@Then("the container is created with no content in the database and message displayed saying {string}")
+	public void the_container_is_created_with_no_content_in_the_database_and_message_displayed_saying(String string) {
+		C = new Container(enviro,loc);
+		String s = C.toString();
+		d.addToDatabase("Containers", s);
+		System.out.print(string);
+	}
+	@Then("container is found in the database")
+	public void container_is_found_in_the_database(){
+		
+	}
+}
