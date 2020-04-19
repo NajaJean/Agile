d63c3b2b5ac4eacd8327c5c584c2ae0d504d3e51
NajaJean <naja-jean@live.dk>
2020-04-19 08:43:08 +0200

    LogisticReg

---
+++ src/stepDefinitions/StepDefs_LogisticRegisterContainer2.java
@@ -0,0 +1,32 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefs_LogisticRegisterContainer2 {
+
+	@Given("that Logistic Company wants to register a container")
+	public void that_Logistic_Company_wants_to_register_a_container() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("the Logistic Company inserts all information")
+	public void the_Logistic_Company_inserts_all_information() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the container is created with no content in the database and message displayed saying {string}")
+	public void the_container_is_created_with_no_content_in_the_database_and_message_displayed_saying(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("container is found in the database")
+	public void container_is_found_in_the_database() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
