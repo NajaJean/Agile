bcbfe34ec28e5da095c498c8cedfb01038712a19
cartergran <cartergran@users.noreply.github.com>
2020-04-22 15:42:37 -0700

    Slight UI changes - centering

--- src/UI/StartLoginPage.java
+++ src/UI/StartLoginPage.java
@@ -1,4 +1,8 @@
 package UI;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
 import javax.swing.*;
 
 public class StartLoginPage extends JFrame {
@@ -6,27 +10,33 @@ public class StartLoginPage extends JFrame {
     JLabel welcomeLabel = new JLabel ("Welcome to ContainerSystem");
     JButton lcLogin = new JButton ("Logistic Company Login");
     JButton cLogin = new JButton ("Client Login");
+    
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private int frameWidth = 400;
+    private int frameHeight = 200;
  
     public StartLoginPage() {
-        setLayoutManager();
+        setLayout(null);
         setLocationAndSize();
         addComponentsToContainer();
         setTitle("Choose Login");
         setVisible(true);
-        setBounds(10, 10, 370, 600);
+        
+        // Center JFrame in the middle of screen when it is initialized
+        setBounds(screenSize.width/2, screenSize.height/2, frameWidth, frameHeight);
+        setLocationRelativeTo(null);
+        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
     }
- 
-    public void setLayoutManager() {
-        container.setLayout(null);
-    }
     
     public void setLocationAndSize() {
-    	welcomeLabel.setBounds (45, 5, 195, 25);
-        lcLogin.setBounds (35, 35, 200, 25);
-        cLogin.setBounds (85, 65, 100, 25);
-    }
+    	// Set all JComponents into the center of the JFrame
+    	welcomeLabel.setFont(new Font("Serif", Font.BOLD, 16));
+    	welcomeLabel.setBounds ((frameWidth/2) - (200/2), 10, 200, 25);
+        lcLogin.setBounds ((frameWidth/2) - (200/2), 35, 200, 50);
+        cLogin.setBounds ((frameWidth/2) - (100/2), 85, 100, 50);
+    } 
  
     public void addComponentsToContainer() {
     	container.add(welcomeLabel);
