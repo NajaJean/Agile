f038ec43513b884ddff6df50035fd24fefc70548
Mathilde Tannebæk <s184376@student.dtu.dk>
2020-04-23 14:24:31 +0200

    LogisticCompanyMenu controller implementation

---
+++ src/controllers/LogisticCompanyMenuController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import UI.LogisticCompanyMenu;
+import UI.StartLoginPage;
+
+public class LogisticCompanyMenuController {
+	LogisticCompanyMenu view;
+	
+	public LogisticCompanyMenuController() {
+		view = new LogisticCompanyMenu();
+	}
+	
+	public void initController() {
+		view.getUpdateContainerStatusItem().addActionListener(e -> openUpdateContainerStatusMenu());
+		view.getRegisterContainerItem().addActionListener(e -> openRegisterNewContainerMenu());
+		view.getCreateNewClientItem().addActionListener(e -> goToCreateClientMenu());
+		view.getSignOutItem().addActionListener(e -> signOut());
+	}
+	
+	private void signOut() {
+		view.dispose();
+		StartLoginPage view = new StartLoginPage();
+		StartLoginPageController w = new StartLoginPageController(view);
+		w.initController();
+	}
+	
+	private void openUpdateContainerStatusMenu() {
+		System.out.println("not yet implemented");
+	}
+	
+	private void openRegisterNewContainerMenu() {
+		System.out.println("not yet implemented");
+	}
+	
+	private void goToCreateClientMenu() {
+		System.out.println("not yet implemented");
+	}
+}
