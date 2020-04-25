b3b0958e9a018aa92fe08804d097fb529c8d2136
Andrew <62332857+Andrew00000@users.noreply.github.com>
2020-04-25 08:53:34 +0200

    map update

--- src/controllers/ClientMenuController.java
+++ src/controllers/ClientMenuController.java
@@ -2,23 +2,31 @@ package controllers;
 
 import UI.ClientMenu;
 import UI.ConfigureClient;
+import UI.Map;
 import UI.StartLoginPage;
 import core.Client;
+import core.ContainerJourney;
+import core.DatabaseData;
 
 public class ClientMenuController {
 	private ClientMenu view; 
 	Client client;
+	ContainerJourney[] cJs;
 	
 	ClientMenuController(Client client) {
 		view = new ClientMenu();
 		this.client = client;
+		this.cJs = DatabaseData.getJournies();
 	}
 	
 	public void initController() {
 		view.getBook_containerItem().addActionListener(e -> goToBookContainerMenu());
 		view.getSign_outItem().addActionListener(e -> signOut());
 		view.getConfigure_client_detailsItem().addActionListener(e -> goToConfigureClientMenu());
+		view.getShowMyContainersItem().addActionListener(e -> gotoShowMyContainersItem());
 	}
+	
+
 	
 	private void goToBookContainerMenu() {
 		
@@ -35,5 +43,14 @@ public class ClientMenuController {
 		view.dispose();
 		ConfigureClientController wConf = new ConfigureClientController(client);
 		wConf.initController();
+	}
+	
+	private void gotoShowMyContainersItem() {
+		System.out.println(cJs[0].toString());
+		System.out.println(cJs[1].toString());
+		System.out.println(cJs[2].toString());
+		Map map = new Map(cJs);
+		map.showAllContainers();
+		
 	}
 }
--- src/controllers/MapController.java
+++ src/controllers/MapController.java
@@ -1,5 +1,44 @@
 package controllers;
 
+import javax.swing.JOptionPane;
+
+import UI.Login;
+import UI.StartLoginPage;
+import core.Client;
+import core.Container;
+import core.ContainerJourney;
+import core.Content;
+import core.Database;
+import core.Environment;
+import core.Location;
+import core.LogisticCompany;
+import core.NotifyObject;
+
 public class MapController {
 
-}
+	Database d; 
+	Client[] Clients;
+	Environment[] Enviros;
+	Content[] Contents;
+	Container[] Containers; 
+	Location[] Locations;
+	ContainerJourney[] Journies;
+	
+	Client c;
+	LogisticCompany l = new LogisticCompany();
+	NotifyObject response;
+
+	
+	public MapController( Client[] Clients, Environment[] Enviros, Content[] Contents,
+						  Container[] Containers, Location[] Locations, ContainerJourney[] Journies,
+						  Database d) 
+	{
+		this.Clients = Clients;
+		this.Enviros = Enviros;
+		this.Contents = Contents;
+		this.Containers = Containers;
+		this.Locations = Locations;
+		this.Journies = Journies;
+		this.d = d;
+	}
+}	
