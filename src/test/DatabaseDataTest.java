5f3b45ce714abfb93891f52841fe5a190157d8a2
cartergran <cartergran@users.noreply.github.com>
2020-04-26 21:36:38 -0700

    DatabaseData JUnit test

---
+++ src/test/DatabaseDataTest.java
@@ -0,0 +1,78 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import core.Client;
+import core.Container;
+import core.ContainerJourney;
+import core.Content;
+import core.DatabaseData;
+import core.Environment;
+import core.Location;
+
+public class DatabaseDataTest {
+	Client client;
+	
+	Content content;
+	Location startLocation;
+	Container container;
+	
+	Location endLocation;
+	ContainerJourney containerJourney;
+	
+	@Before
+	public void init() {
+		client = new Client("M", "1234", "Mathilde","mathildesemail@gmail.com","Anker Egelundsvej 1");
+		
+		content = new Content("Apple", new Environment(2.0,0.8,0.5), 0.1);
+		startLocation = new Location("Copenhagen", new double[]{55.68, 12.57});
+		container = new Container(client, content, startLocation);
+		
+		endLocation = new Location("New York", new double[]{40.71, 74.01});
+		containerJourney = new ContainerJourney(startLocation, endLocation, container);
+	}
+
+	@Test
+	public void addDataTest() {
+		// Add client
+		DatabaseData.addClient(client);
+		assertEquals(client, DatabaseData.getClients()[DatabaseData.getClients().length - 1]);
+		
+		// Add container
+		DatabaseData.addContainer(container);
+		assertEquals(container, DatabaseData.getContainers()[DatabaseData.getContainers().length - 1]);
+		
+		// Add container journey
+		DatabaseData.addContainerJourney(containerJourney);
+		assertEquals(containerJourney, DatabaseData.getJournies()[DatabaseData.getJournies().length - 1]);
+	}
+	
+	@Test
+	public void dataObserverTest() {
+		int oldLengthClients = DatabaseData.getClients().length;
+		Client newClient = new Client("C", "2323", "Carter", "carter@carter.com", "San Luis Obispo");
+		DatabaseData.getDatabase().addToDatabase("Clients", newClient.toString(), newClient);
+		
+		assertEquals((oldLengthClients + 1), DatabaseData.getClients().length);
+		assertEquals(newClient, DatabaseData.getClients()[oldLengthClients]);
+		
+		int oldLengthContainers = DatabaseData.getContainers().length;
+		Container C = new Container(DatabaseData.getLocations()[1]);
+		DatabaseData.getDatabase().addToDatabase("Containers", C.toString(), C);
+		
+		assertEquals((oldLengthContainers + 1), DatabaseData.getContainers().length);
+		assertEquals(C, DatabaseData.getContainers()[oldLengthContainers]);
+		
+		/*
+		int oldLengthContainerJourney = DatabaseData.getJournies().length;
+		ContainerJourney newContainerJourney = new ContainerJourney(startLocation, endLocation, newContainer);
+		DatabaseData.getDatabase().addToDatabase("Journies", newContainerJourney.toString(), newContainerJourney);
+		
+		assertEquals((oldLengthContainerJourney + 1), DatabaseData.getJournies().length);
+		assertEquals(newContainerJourney, DatabaseData.getJournies()[oldLengthContainerJourney]);
+		*/
+	}
+}
