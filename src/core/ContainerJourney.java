d4c997d8d7c51433265b4fc6fa0bdbc46f0f30d9
Samantha Swift <62332864+17swifts@users.noreply.github.com>
2020-04-28 20:40:30 +1000

    More business logic updates
    New constructor for jounies when reading in from database - have not yet run junit tests although the old contrustor will still be used when creating containers.

--- src/core/ContainerJourney.java
+++ src/core/ContainerJourney.java
@@ -30,6 +30,16 @@ public class ContainerJourney {
 		currentGps[1] = temp[1];
 	}
 	
+	public ContainerJourney(Location start, Location end, Container container,double currentx, double currenty) {
+		// Overloaded method just when reading in the database 
+		this.journeyID = count++;
+		this.start = start;
+		this.end = end;
+		this.container = container;
+		currentGps[0] = currentx;
+		currentGps[1] = currenty;
+	}
+	
 	public int getJourneyID() {
 		return journeyID;
 	}
--- src/core/DatabaseData.java
+++ src/core/DatabaseData.java
@@ -122,7 +122,8 @@ public class DatabaseData {
 		Journies = new ContainerJourney[journiesLength];
 		for(int i = 0; i < journiesLength; i++) {
 			Journies[i] = new ContainerJourney(Location.findLocation(Integer.parseInt(journies[i+1][1]), Locations), 
-				Location.findLocation(Integer.parseInt(journies[i+1][2]), Locations), Container.findContainer(Integer.parseInt(journies[i+1][3]), Containers));	
+				Location.findLocation(Integer.parseInt(journies[i+1][2]), Locations), Container.findContainer(Integer.parseInt(journies[i+1][3]), Containers),
+				Double.parseDouble(journies[i+1][6]),Double.parseDouble(journies[i+1][5]));	
 		}
 	}
 	
