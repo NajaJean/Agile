527d5cb4ebec3c38d5b5fabb59364dbc8bd9f3c0
Samantha Swift <62332864+17swifts@users.noreply.github.com>
2020-05-05 11:21:34 +1000

    Update ContainerJourneyTest.java

--- src/test/ContainerJourneyTest.java
+++ src/test/ContainerJourneyTest.java
@@ -32,21 +32,6 @@ public class ContainerJourneyTest {
 		Container[] containers = DatabaseData.getContainers();
 		
 		ContainerJourney[] emptyCJs = new ContainerJourney[0];
-/*		Client client = new Client("M", "1234", "Mathilde","mathildesemail@gmail.com","Anker Egelundsvej 1");
-		Environment environment = new Environment(5.3,1.1,0.85);
-		Content content = new Content("Banana",new Environment(5.0,1.0,0.85), 0.1);
-		double[] cphgps = {55.1, 12.6};
-		Location loc = new Location("Copenhagen",cphgps);
-		
-		Container c = new Container(client, environment, content,loc);
-		
-		double[] gpsStart = {55.67594, 12.56553};
-		double[] gpsEnd = {52.370216, 4.895168};
-		
-		Location start = new Location("Copenhagen", gpsStart);
-		Location end = new Location("Amsterdam", gpsEnd);
-		
-		ContainerJourney cj = new ContainerJourney(start, end, c); */
 		
 		assertEquals(1, cJ[0].getJourneyID());
 		
@@ -74,35 +59,4 @@ public class ContainerJourneyTest {
 		assertEquals(-1, search.findIDX("29", cJ));
 		assertEquals(-1, search.findIDX("4", emptyCJs));
 	}
-	
-/*		@Test
-		public void testFindJourney() {
-			Client client = new Client("M", "1234", "Mathilde","mathildesemail@gmail.com","Anker Egelundsvej 1");
-			Environment environment = new Environment(5.3,1.1,0.85);
-			Content content = new Content("Banana",new Environment(5.0,1.0,0.85), 0.1);
-			double[] cphgps = {55.1, 12.6};
-			Location loc = new Location("Copenhagen",cphgps);
-			
-			Container c = new Container(client, environment, content,loc);
-			
-			double[] gpsStart = {55.67594, 12.56553};
-			double[] gpsEnd = {52.370216, 4.895168};
-			
-			Location start = new Location("Copenhagen", gpsStart);
-			Location end = new Location("Amsterdam", gpsEnd);
-			
-			ContainerJourney cStart = new ContainerJourney(start, end, c);
-			ContainerJourney cEnd = new ContainerJourney(end, start, c);
-			ContainerJourney[] cjArray = {cStart, cEnd};
-			ContainerJourney[] cjArrayEmpty = null;
-			
-//			assertEquals("'1', ''Copenhagen', '2', '55.67594', '12.56553'', ''Amsterdam', '3', '52.370216', '4.895168'', " + "''1', '1', '1', '1',"	+ " '1'', '55.67594', '12.56553'", cStart.toString());
-			assertEquals("'1', '2', '3', '1', '55.67594', '12.56553'", cStart.toString());
-			
-			assertEquals(cStart, ContainerJourney.findJourney("1", cjArray));
-			assertEquals(null, ContainerJourney.findJourney("99", cjArray));
-			assertEquals(null, ContainerJourney.findJourney("4", cjArrayEmpty));
-			
-			
-		}*/
 }
