2651cc55c1ebc5e119d9f508ac2c40def08076fb
Samantha Swift <62332864+17swifts@users.noreply.github.com>
2020-05-09 21:31:21 +1000

    Update LogisticUpdateController.java

--- src/controllers/LogisticUpdateController.java
+++ src/controllers/LogisticUpdateController.java
@@ -20,11 +20,12 @@ public class LogisticUpdateController {
 	ContainerJourney[] cJs;
 	LogisticUpdate view; 
 	String selectedContainer = "";
-	
+
 	Container chosenContainer;
-	
+	ContainerJourney chosenCJ;
+
 	ArraySearch search;
-	
+
 	public LogisticUpdateController() {
 		Containers = DatabaseData.getContainers();//possibly never used, check this else delete
 		cJs = DatabaseData.getJournies();
@@ -32,106 +33,106 @@ public class LogisticUpdateController {
 		this.view = new LogisticUpdate(cJs);
 		this.search = new ArraySearch();
 	}
-	
+
 	public void initController() {
 		view.getcancelButton().addActionListener(e -> cancel());
 		view.getsaveButton().addActionListener(e -> updateContainer());	
 		JCheckBox checkBox = view.getarrivedCheckBox();
 		JComboBox containerBox = view.getcontainerBox();
-		
+
 		containerBox.addActionListener(new ActionListener() {
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		    	if (!selectedContainer.contentEquals("")) {
-		    		view.getgpsLatitudeField().setText("");
-	    			view.getgpsLongitudeField().setText("");
-					
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!selectedContainer.contentEquals("")) {
+					view.getgpsLatitudeField().setText("");
+					view.getgpsLongitudeField().setText("");
+
 					view.getgpsLatitudeField().setEnabled(true);
 					view.getgpsLongitudeField().setEnabled(true);
 					checkBox.setSelected(false);
 				}
-		    	selectedContainer = containerBox.getSelectedItem().toString();
-		    	
-		    	search.setSearch(new Container());
-		    	int containerIDX = search.findIDX(Integer.parseInt(selectedContainer), Containers);
-		    	if (containerIDX == -1) {
-		    		JOptionPane.showMessageDialog(null, "Error updating container");
-		    		return;
-		    	}
-		        chosenContainer = Containers[containerIDX];    
-		    }
+				selectedContainer = containerBox.getSelectedItem().toString();
+
+				search.setSearch(new Container());
+				int containerIDX = search.findIDX(Integer.parseInt(selectedContainer), Containers);
+				if (containerIDX == -1) {
+					JOptionPane.showMessageDialog(null, "Error updating container");
+					return;
+				}
+				chosenContainer = Containers[containerIDX]; 
+				search.setSearch(new ContainerJourney());
+				int cjIDX = search.findIDX(String.valueOf(chosenContainer.getContainerID()), cJs); // CJ found from containerID
+				if (cjIDX == -1) {
+					JOptionPane.showMessageDialog(null, "Error updating container");
+					return;
+				}
+				chosenCJ = cJs[cjIDX];
+			}
 		});
-		
+
 		checkBox.addActionListener(new ActionListener() {
-		      public void actionPerformed(ActionEvent actionEvent) {
-		        AbstractButton abstractButton = (AbstractButton) actionEvent.getSource();
-		        boolean selected = abstractButton.getModel().isSelected();
-		        if (selectedContainer.equals("")) {
-		        	containerNotSelected();
-		        	checkBox.setSelected(false);
-		        }
-		        else {
-		        	if (selected) {
-			        	view.getgpsLatitudeField().setText(Double.toString(chosenContainer.getContainerLocation().getGPScoordX()));
-						view.getgpsLongitudeField().setText(Double.toString(chosenContainer.getContainerLocation().getGPScoordY()));
-						
+			public void actionPerformed(ActionEvent actionEvent) {
+				AbstractButton abstractButton = (AbstractButton) actionEvent.getSource();
+				boolean selected = abstractButton.getModel().isSelected();
+				if (selectedContainer.equals("")) {
+					containerNotSelected();
+					checkBox.setSelected(false);
+				}
+				else {
+					if (selected) {
+						view.getgpsLatitudeField().setText(Double.toString(chosenCJ.getEndLocX()));
+						view.getgpsLongitudeField().setText(Double.toString(chosenCJ.getEndLocY()));
+
 						view.getgpsLatitudeField().setEnabled(false);
 						view.getgpsLongitudeField().setEnabled(false);
-			        }
-		        	else {
-		        		view.getgpsLatitudeField().setText("");
-		    			view.getgpsLongitudeField().setText("");
-						
+					}
+					else {
+						view.getgpsLatitudeField().setText("");
+						view.getgpsLongitudeField().setText("");
+
 						view.getgpsLatitudeField().setEnabled(true);
 						view.getgpsLongitudeField().setEnabled(true);
-		        	}
-		        }
-		      }
-		    });
+					}
+				}
+			}
+		});
 	}
-	
+
 	public void cancel() {
 		LogisticCompanyMenuController lm = new LogisticCompanyMenuController();
 		view.dispose();
 		lm.initController();
-		
+
 	}
-	
+
 	public void updateContainer() {
-		
+
 		try {view.getgpsLatitudeField().setEnabled(true);
-			 view.getgpsLongitudeField().setEnabled(true);
-			 
-			 String gpsLatitudeText = view.getgpsLatitudeField().getText();
-			 String gpsLongitudeText = view.getgpsLongitudeField().getText();
-			 
-			 double gpsLatitude = Double.parseDouble(gpsLatitudeText);
-			 double gpsLongitude = Double.parseDouble(gpsLongitudeText);
-			 
-			 search.setSearch(new ContainerJourney());
-			 int cjIDX = search.findIDX(String.valueOf(chosenContainer.getContainerID()), cJs); // CJ found from containerID
-			 if (cjIDX == -1) {
-				 JOptionPane.showMessageDialog(null, "Error updating container");
-				 return;
-			 }
-			 ContainerJourney containerJourney = cJs[cjIDX];
-			 containerJourney.setCurrentLocation(new double [] {gpsLatitude, gpsLongitude});
-			 
-			 DatabaseData.getDatabase().updateDatabase("Journies", "Current_x", Double.toString(gpsLatitude), Integer.toString(containerJourney.getJourneyID()));
-			 DatabaseData.getDatabase().updateDatabase("Journies", "Current_y", Double.toString(gpsLongitude), Integer.toString(containerJourney.getJourneyID()));			
-			 DatabaseData.getDatabase().updateDatabase("Journies", "EndDate", Calendar.getSystemDate().toString(), Integer.toString(containerJourney.getJourneyID()));			
-			 
-			 //If container has arrived
-			 //Set the container client ID and content ID to null (make it free for a new journey) and update its current location
-			 DatabaseData.getDatabase().updateDatabase("Containers", "Client_ID",Integer.toString(containerJourney.getContaineronJourney().getContainerID()));
-			 DatabaseData.getDatabase().updateDatabase("Containers", "Content_ID",Integer.toString(containerJourney.getContaineronJourney().getContainerID()));
-			 DatabaseData.getDatabase().updateDatabase("Containers", "Location_ID",Integer.toString(containerJourney.getEndLocation().getLocationID()),Integer.toString(containerJourney.getContaineronJourney().getContainerID()));
-			 
-			 updateSuccessful();
-			
-			 LogisticCompanyMenuController lm = new LogisticCompanyMenuController();
-			 view.dispose();
-			 lm.initController();	 
+		view.getgpsLongitudeField().setEnabled(true);
+
+		String gpsLatitudeText = view.getgpsLatitudeField().getText();
+		String gpsLongitudeText = view.getgpsLongitudeField().getText();
+
+		double gpsLatitude = Double.parseDouble(gpsLatitudeText);
+		double gpsLongitude = Double.parseDouble(gpsLongitudeText);
+
+		chosenCJ.setCurrentLocation(new double [] {gpsLatitude, gpsLongitude});
+
+		DatabaseData.getDatabase().updateDatabase("Journies", "Current_x", Double.toString(gpsLatitude), Integer.toString(chosenCJ.getJourneyID()));
+		DatabaseData.getDatabase().updateDatabase("Journies", "Current_y", Double.toString(gpsLongitude), Integer.toString(chosenCJ.getJourneyID()));			
+		DatabaseData.getDatabase().updateDatabase("Journies", "EndDate", Calendar.getSystemDate().toString(), Integer.toString(chosenCJ.getJourneyID()));			
+
+		//If container has arrived
+		//Set the container client ID and content ID to null (make it free for a new journey) and update its current location
+		DatabaseData.getDatabase().updateDatabase("Containers", "Client_ID",Integer.toString(chosenCJ.getContaineronJourney().getContainerID()));
+		DatabaseData.getDatabase().updateDatabase("Containers", "Content_ID",Integer.toString(chosenCJ.getContaineronJourney().getContainerID()));
+		DatabaseData.getDatabase().updateDatabase("Containers", "Location_ID",Integer.toString(chosenCJ.getEndLocation().getLocationID()),Integer.toString(chosenCJ.getContaineronJourney().getContainerID()));
+
+		updateSuccessful();
+
+		LogisticCompanyMenuController lm = new LogisticCompanyMenuController();
+		view.dispose();
+		lm.initController();	 
 		}
 		catch (Exception e) {
 			if (selectedContainer.equals("")) {
@@ -142,16 +143,16 @@ public class LogisticUpdateController {
 			}
 		}			
 	}
-	
+
 	public void containerNotSelected() {
 		JOptionPane.showMessageDialog(null, "Please select a container to update");
 	}
 	public void GPSisWrong() {
 		JOptionPane.showMessageDialog(null, "Error: GPS Latitude and GPS Longitude must "
-										+ "\nbe given as decimal numbers.");
+				+ "\nbe given as decimal numbers.");
 	}
 	public void updateSuccessful() {
 		JOptionPane.showMessageDialog(null, "Container successfully updated!");
 	}
-	
+
 }
